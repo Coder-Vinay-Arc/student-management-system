6bfbb7f53ebc54164a06c389d9faafb1a7b771b8
Vinay Gupta <vinayjee04@gmail.com>
2026-07-23 15:55:59 +0530

    cors problem fixed

--- backend/src/main/java/com/vinay/studentmanagement/controller/StudentController.java
+++ backend/src/main/java/com/vinay/studentmanagement/controller/StudentController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://student-management-system-tau-blush.vercel.app"
+})
 @RestController
 @RequestMapping("/students")
 
